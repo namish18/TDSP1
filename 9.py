e044e9856908055937ea627582544651572169cc
namish18 <114792370+namish18@users.noreply.github.com>
2024-11-02 09:30:21 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
+
+# Read the CSV files
+users = pd.read_csv('users.csv')
+repos = pd.read_csv('repositories.csv')
 users['leader_strength'] = users['followers'] / (1 + users['following'])
 top_5_leaders = users.nlargest(5, 'leader_strength')['login'].tolist()
 print("\nTop 5 users by Leader Strength:")
